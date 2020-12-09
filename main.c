ff2c39df7cae9e645eb1112c8c0b881ff065a81c
jogefuda <71481387+jogefuda@users.noreply.github.com>
2020-12-09 15:27:22 +0800

    impl: auth
    update main
    
    remove unused file

--- main.c
+++ main.c
@@ -15,20 +15,12 @@ int main(int argc, char *argv[]) {
         .ui_name = "Hello"
     };
 
-    // mc_eventloop(si);
     // mc_getinfo(si, MC_REQ_SPL);
-    // mc_login(si, &ui);
+    mc_login(si, &ui);
+    read_packet(si, 0, 0);
     // sleep(1); // TODO: wait until connection change to play state
-    // read_packet(si, 0, 0);
     // mc_chat(si, "Cursed world");
 
-    char out[42] = { 0 };
-    void *ctx = mc_hash_init(NULL);
-    mc_hash_update(ctx, "jeb_", 4);
-    mc_hash_final(ctx, out, 40);
-    printf("%s\n", out);
-
-    getchar();
     mc_cleanup(si);
     return 0;
 }
