70292adfc32fd0b14ab905bc29901b733a8b8693
jogefuda <71481387+jogefuda@users.noreply.github.com>
2020-12-13 19:20:31 +0800

    remove unused function

--- utils.h
+++ utils.h
@@ -6,12 +6,6 @@
 #define M_FAIL 1
 #define M_SUCCESS 0
 
-typedef struct bytearray {
-    size_t b_size;
-    size_t b_allocsize;
-    char *b_data;
-} bytearray_t;
-
 typedef struct buffer {
     size_t b_size;
     size_t b_allocsize;
@@ -19,12 +13,8 @@ typedef struct buffer {
     char *b_next;
 } buffer_t;
 
-void *inc_bytearray(struct bytearray *arr, size_t size);
-struct bytearray *new_bytearray(size_t len);
-void del_bytearray(struct bytearray *ptr);
-
-int inc_buffer_if_not_enough(struct buffer *arr, size_t size);
 void *inc_buffer(struct buffer *arr, size_t size);
+int inc_buffer_if_not_enough(struct buffer *arr, size_t size);
 struct buffer *new_buffer(size_t len);
 void del_buffer(struct buffer *ptr);
 
